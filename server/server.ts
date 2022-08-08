e283c55bd8cdcea4211bd084ebebb5cc46bb617a
Milosz B <miloszbargiel@gmail.com>
2022-08-08 18:39:55 +0200

    Fix cannot set headers error in error handling function

--- server/server.ts
+++ server/server.ts
@@ -20,10 +20,12 @@ app.use((err: apiError, req: express.Request, res: express.Response, next: NextF
   if (err.status === 400) {
     res.status(400);
     res.send(err);
+  } else {
+    res.status(404);
+    const noData = {status: 404, message: 'Not found'};
+    res.send(noData);
   }
-  res.status(404);
-  const noData = {status: 404, message: 'Not found'};
-  res.send(noData);
+  
 })
 
 app.listen(8000, () => {
