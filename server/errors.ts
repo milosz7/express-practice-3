554c9e37137db8f874f9f332e63ee7892d52afb9
Milosz B <miloszbargiel@gmail.com>
2022-08-09 12:23:47 +0200

    Extract error objects into a separate file

---
+++ server/errors.ts
@@ -0,0 +1,14 @@
+export const notFoundErr = {
+  status: 404,
+  message: 'Not found.'
+};
+
+export const badRequestErr = {
+  status: 400,
+  message: 'Please provide all necessary data!'
+};
+
+export const conflictErr = {
+  status: 409,
+  message: 'The slot is already taken!'
+}
