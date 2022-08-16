9cc324819c926f6385938c605a8ec24b5c53bc3c
Milosz B <miloszbargiel@gmail.com>
2022-08-16 11:32:35 +0200

    Add socket connection url

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
 export const API_URL = (process.env.NODE_ENV === 'production') ? '/api' : 'http://localhost:8000/api';
+export const SOCKET_URL = process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:8000';
