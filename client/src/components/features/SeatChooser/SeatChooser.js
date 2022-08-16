53ccd7cb4939b943f9ecbc52a44e062f347b884b
Milosz B <miloszbargiel@gmail.com>
2022-08-16 11:31:42 +0200

    Add client side socket logic

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -1,48 +1,98 @@
 import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { Button, Progress, Alert } from 'reactstrap';
-import { getSeats, loadSeatsRequest, getRequests } from '../../../redux/seatsRedux';
 import './SeatChooser.scss';
+import { io } from 'socket.io-client';
+import { SOCKET_URL } from '../../../config';
+
+export const socket = io(SOCKET_URL, { autoConnect: false });
 
 const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
-  const dispatch = useDispatch();
-  const seats = useSelector(getSeats);
-  const requests = useSelector(getRequests);
-  const [intervalStatus, setIntervalStatus] = useState(null);
-  const twoMinutesInMs = 120000;
+  const [seats, setSeats] = useState([]);
+  const [error, setError] = useState(false);
+  const [isConnected, setIsConnected] = useState(socket.connected);
 
   useEffect(() => {
-    dispatch(loadSeatsRequest());
-      setIntervalStatus(setInterval(() => {
-        dispatch(loadSeatsRequest());
-      }, twoMinutesInMs))
+    socket.connect();
+    socket.on('connect', async () => {
+      if (error) {
+        setError(false);
+      }
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('updateData', (seats) => {
+      setSeats(seats);
+    });
+
+    socket.on('connect_error', () => {
+      setError(true);
+    });
 
     return () => {
-      clearInterval(intervalStatus);
-      setIntervalStatus(null);
-    }
-  }, [dispatch])
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('updateData');
+      socket.off('connect_error');
+    };
+  }, []);
 
   const isTaken = (seatId) => {
-    return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
-  }
+    return seats.some((item) => item.seat === seatId && item.day === chosenDay);
+  };
 
   const prepareSeat = (seatId) => {
-    if(seatId === chosenSeat) return <Button key={seatId} className="seats__seat" color="primary">{seatId}</Button>;
-    else if(isTaken(seatId)) return <Button key={seatId} className="seats__seat" disabled color="secondary">{seatId}</Button>;
-    else return <Button key={seatId} color="primary" className="seats__seat" outline onClick={(e) => updateSeat(e, seatId)}>{seatId}</Button>;
-  }
+    if (seatId === chosenSeat)
+      return (
+        <Button key={seatId} className="seats__seat" color="primary">
+          {seatId}
+        </Button>
+      );
+    else if (isTaken(seatId))
+      return (
+        <Button key={seatId} className="seats__seat" disabled color="secondary">
+          {seatId}
+        </Button>
+      );
+    else
+      return (
+        <Button
+          key={seatId}
+          color="primary"
+          className="seats__seat"
+          outline
+          onClick={(e) => updateSeat(e, seatId)}
+        >
+          {seatId}
+        </Button>
+      );
+  };
 
   return (
     <div>
       <h3>Pick a seat</h3>
-      <small id="pickHelp" className="form-text text-muted ml-2"><Button color="secondary" /> – seat is already taken</small>
-      <small id="pickHelpTwo" className="form-text text-muted ml-2 mb-4"><Button outline color="primary" /> – it's empty</small>
-      { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
-      { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
-      { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
+      <small id="pickHelp" className="form-text text-muted ml-2">
+        <Button color="secondary" /> – seat is already taken
+      </small>
+
+      <small id="pickHelpTwo" className="form-text text-muted ml-2 mb-4">
+        <Button outline color="primary" /> – it's empty
+      </small>
+      {(isConnected && !error) &&
+      <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i + 1))}</div>
+      }
+      {(!isConnected && !error) &&
+      <Progress animated color="primary" value={50} />
+      }
+      {error &&
+      <Alert color="warning">Couldn't load seats...</Alert>
+      }
     </div>
-  )
-}
+  );
+};
 
 export default SeatChooser;
