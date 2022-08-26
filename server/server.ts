f99ed9e091749afebf417d5fa37e3e7b1181e615
Milosz B <miloszbargiel@gmail.com>
2022-08-26 17:10:15 +0200

    Update error handling middleware function

--- server/server.ts
+++ server/server.ts
@@ -3,28 +3,28 @@ import testimonialRoutes from './routes/testimonials.routes';
 import concertsRoutes from './routes/concerts.routes';
 import seatsRoutes from './routes/seats.routes';
 import cors from 'cors';
-import { notFoundErr } from './errors'
-import path from 'path'
+import { notFoundErr } from './errors';
+import path from 'path';
 import { Server } from 'socket.io';
 import mongoose from 'mongoose';
 
 interface apiError {
   status: number;
-  message: string
+  message: string;
 }
 
 const app = express();
 
-const uri = 'mongodb://127.0.0.1:27017/NewWaveDB'
+const uri = 'mongodb://127.0.0.1:27017/NewWaveDB';
 
-mongoose.connect(uri)
-const db = mongoose.connection
+mongoose.connect(uri);
+const db = mongoose.connection;
 
 db.once('open', () => {
-  console.log('Connected to database.')
+  console.log('Connected to database.');
 });
 
-db.on('error', (e: Error) => console.log(e))
+db.on('error', (e: Error) => console.log(e));
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Running on port 8000');
@@ -34,36 +34,29 @@ const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
-  }
+  },
 });
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(cors());
 app.use((req, res, next) => {
   req.io = io;
   next();
-})
+});
 app.use('/api', testimonialRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
 app.use((err: apiError, req: express.Request, res: express.Response, next: NextFunction) => {
-  if (err.status) {
-    res.status(err.status);
-    return res.send(err.message);
-  }
-    res.status(404);
-    res.send(notFoundErr);
-  });
+  return res.json(err.message);
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
-
 io.on('connection', (socket) => {
-  // socket.emit('updateData', db)
+  // socket.emit('updateData', db.seats)
 });
